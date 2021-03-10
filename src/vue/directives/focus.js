9c1b076c7a42cec24da2aed61ce23a64b5c17883
Allen Garvey <allengarvey@hotmail.com>
2021-03-09 20:10:34 -0500

    Remove unused chess board canvas code

--- src/vue/directives/focus.js
+++
@@ -1,9 +0,0 @@
-//from https://stackoverflow.com/questions/34941829/setting-focus-of-an-input-element-in-vue-js
-//usage: <input v-focus>
-
-export default{
-    name: 'focus',
-    mounted(el){
-        el.focus();
-    }
-};
