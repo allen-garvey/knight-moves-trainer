56fa44198699b4d10af2c18a72171b2c2fc3af05
Allen Garvey <allengarvey@hotmail.com>
2021-03-11 14:53:36 -0500

    Add pathfinding for queen diagonal

--- src/pathfinding.js
+++ src/pathfinding.js
@@ -7,9 +7,9 @@ const numberToCoordinate = (number) => {
     };
 };
 
-// const coordinateToNumber = (x, y) => {
-//     return y * DIMENSIONS + x;
-// };
+const coordinateToNumber = (x, y) => {
+    return y * DIMENSIONS + x;
+};
 
 const queenSquares = (queenNumber) => {
     const ret = {
@@ -17,12 +17,33 @@ const queenSquares = (queenNumber) => {
     };
     const {x: queenX, y: queenY} = numberToCoordinate(queenNumber);
     
+    // horizontal and vertical
     for(let y=0,number=0;y<DIMENSIONS;y++){
         for(let x=0;x<DIMENSIONS;x++,number++){
             if(x === queenX || y === queenY){
                 ret[number] = true;
             }
         }
+    }
+    
+    //diagonal to the lower right
+    for(let y=queenY+1,x=queenX+1;y<DIMENSIONS&&x<DIMENSIONS;y++,x++){
+        ret[coordinateToNumber(x, y)] = true;
+    }
+
+    //diagonal to the lower left
+    for(let y=queenY+1,x=queenX-1;y<DIMENSIONS&&x>=0;y++,x--){
+        ret[coordinateToNumber(x, y)] = true;
+    }
+
+    //diagonal to the upper right
+    for(let y=queenY-1,x=queenX+1;y>=0&&x<DIMENSIONS;y--,x++){
+        ret[coordinateToNumber(x, y)] = true;
+    }
+
+    //diagonal to the upper left
+    for(let y=queenY-1,x=queenX-1;y>=0&&x>=0;y--,x--){
+        ret[coordinateToNumber(x, y)] = true;
     }
 
     return ret;
